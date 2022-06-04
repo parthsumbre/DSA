b7f0f2c368f162506d53c9829a41afc132b6f826
Sushilkumar Sahani <sushilkumarsahani41@gmail.com>
2022-06-04 19:29:37 +0530

    sushil-practice

---
+++ 1.Recursion/list_product.py
@@ -0,0 +1,13 @@
+'''
+Write a recursive function that takes a list of numbers as an input and 
+returns the product of all the numbers in the list.
+'''
+
+def list_product(lst): # function
+    if len(lst) == 1: # base case
+        return lst[0] 
+    else:
+        return lst[len(lst)-1] * list_product(lst[:len(lst)-1]) # Recursive
+
+lst = [1,2,3,4,5]
+print(list_product(lst))
